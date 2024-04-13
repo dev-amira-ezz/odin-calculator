f33d6d667595c33c09ede6a628005d7380fe5e31
Amira Ezzeldin <aezz4007@gmail.com>
2024-04-13 13:32:14 +0200

    Create operate function

--- scripts.js
+++ scripts.js
@@ -20,3 +20,20 @@ const multiply = (num1, num2) => {
 const divide = (num1, num2) => {
   return num1 / num2;
 };
+// ======= Operate Function =======
+const operate = (num1, num2, operator) => {
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break;
+  }
+};
