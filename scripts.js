9aee68b6f59584c1bda50c2b28ddeacadd8631a1
Amira Ezzeldin <aezz4007@gmail.com>
2024-04-13 13:29:07 +0200

    Create variables for data storage

--- scripts.js
+++ scripts.js
@@ -1,23 +1,22 @@
 "use strict";
-
+// ======= Data Variables =======
+let num1 = "";
+let num2 = "";
+let operator = "";
 // ======= Math Functions =======
 // Add
 const add = (num1, num2) => {
-    return parseFloat(num1) + parseFloat(num2);
-}
-
+  return parseFloat(num1) + parseFloat(num2);
+};
 // Subtract
 const subtract = (num1, num2) => {
-    return num1 - num2;
-}
-
+  return num1 - num2;
+};
 // Multiply
 const multiply = (num1, num2) => {
-    return num1 * num2;
-}
-
+  return num1 * num2;
+};
 // Divide
 const divide = (num1, num2) => {
-    return num1 / num2;
-}
-
+  return num1 / num2;
+};
