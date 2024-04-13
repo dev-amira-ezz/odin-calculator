1c85b1f9501e7db0eca934df48b38eeb5a599279
Amira Ezzeldin <aezz4007@gmail.com>
2024-04-13 13:24:46 +0200

    Create basic math functions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
 "use strict";
 
-console.log("Hello World!");
+// ======= Math Functions =======
+// Add
+const add = (num1, num2) => {
+    return parseFloat(num1) + parseFloat(num2);
+}
+
+// Subtract
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+// Multiply
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+// Divide
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
